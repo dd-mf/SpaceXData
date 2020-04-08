8e7976f84bbd8d99a642dcc6a0420e6a435926a0
J Rodden <rodden@mac.com>
2020-04-08 08:41:24 -0400

    Initial Commit

---
+++ SpaceXDataTests/SpaceXDataTests.swift
@@ -0,0 +1,34 @@
+//
+//  SpaceXDataTests.swift
+//  SpaceXDataTests
+//
+//  Created by me on 4/8/20.
+//  Copyright © 2020 DD/MF & Associates. All rights reserved.
+//
+
+import XCTest
+@testable import SpaceXData
+
+class SpaceXDataTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
